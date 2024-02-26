dafebed1a0c59816334dfb86e9f4a6fea20c019f
elisagdelope <elisa.gdl95@gmail.com>
2024-02-26 18:48:27 +0100

    FIX: biocChecks code recommendations

--- tests/testthat/test_summarize_pathway_level.R
+++ tests/testthat/test_summarize_pathway_level.R
@@ -9,20 +9,19 @@ pathways <- lapply(pathways, function(s, g) paste0("g", sample(1:g, size = s, re
 names(pathways) <- paste0("pathway", seq_along(pathways))
 
 test_that("summarize_pathway_level works as expected", {
-  # Test for successful aggregation with default parameters
-  result <- summarize_pathway_level(X, pathways)
-  expect_true(!is.null(result), "Should return a non-null result.")
-  
-  # Test for unsuccessful aggregation due to minsize constraint
-  result_short <- summarize_pathway_level(X, pathways, minsize = 101)
-  expect_true(is.null(result_short), "Should return NULL due to minsize constraint.")
-  
-  # Test for successful aggregation with a dimension-reduction score type (e.g., NMF)
-  result_nmf <- summarize_pathway_level(X, pathways, type = "nmf")
-  expect_true(!is.null(result_nmf), "Should return a non-null result for nmf aggregation.")
-  
-  # Test for successful aggregation with a hypothesis test score type (e.g., ttest)
-  result_ttest <- summarize_pathway_level(X, pathways, type = "ttest")
-  expect_true(!is.null(result_ttest), "Should return a non-null result for ttest aggregation.")
-  
+    # Test for successful aggregation with default parameters
+    result <- summarize_pathway_level(X, pathways)
+    expect_true(!is.null(result), "Should return a non-null result.")
+
+    # Test for unsuccessful aggregation due to minsize constraint
+    result_short <- summarize_pathway_level(X, pathways, minsize = 101)
+    expect_true(is.null(result_short), "Should return NULL due to minsize constraint.")
+
+    # Test for successful aggregation with a dimension-reduction score type (e.g., NMF)
+    result_nmf <- summarize_pathway_level(X, pathways, type = "nmf")
+    expect_true(!is.null(result_nmf), "Should return a non-null result for nmf aggregation.")
+
+    # Test for successful aggregation with a hypothesis test score type (e.g., ttest)
+    result_ttest <- summarize_pathway_level(X, pathways, type = "ttest")
+    expect_true(!is.null(result_ttest), "Should return a non-null result for ttest aggregation.")
 })
